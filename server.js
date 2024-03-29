58fa886bdc2431f0c91e8b76ea89e8880ce803d0
locolemat <122878505+locolemat@users.noreply.github.com>
2024-03-29 15:06:46 +0800

    Made barebones server less barebones

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const path = require('path');
 
 PORT = process.env.PORT || 3500
 
+app.get('^/$|/index.html', (req, res)=>{
+    res.sendFile(path.join('views', 'index.html'))
+}) 
+
 app.listen(PORT, ()=>{console.log(`Server listening on port ${PORT}`)})
